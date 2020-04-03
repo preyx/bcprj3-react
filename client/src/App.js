d6428157602d486e0e7fdfc5e228a7e5a5d056e4
jlam <jennylamwork@gmail.com>
2020-04-03 14:33:04 -0700

    pulled bryan's branch and added chatmsg

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import ChatMsg from './components/ChatMsg'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <ChatMsg />
     </div >
   )
 }
