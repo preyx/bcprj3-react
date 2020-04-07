16bb501be8beb7de26bcfda2c7cf20d735fb0ed1
Frank Savage <gotsavaged@gmail.com>
2020-04-07 11:04:09 -0700

    Got LOGIN RENDERING AGAIN

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -6,7 +6,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-constructor(props) {
+constructor(props); {
   super(props);
   this.state = {
     username: "",
@@ -14,7 +14,7 @@ constructor(props) {
     message: "",
     open: false
   };
-  render() {
+  render(); {
     return (
       <div className="App">
         <header className="App-header">
@@ -69,5 +69,8 @@ constructor(props) {
           </Dialog>
         </header>
       </div>
+    
     );
   }
+}
+    
